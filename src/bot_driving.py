19f619fc2a1ee3fc44201ed548dd460712213a52
Michał Kamiński <mkami2002@gmail.com>
2024-06-01 12:17:17 +0200

    bot_driving update

--- src/bot_driving.py
+++ src/bot_driving.py
@@ -2,6 +2,7 @@ import cv2
 import onnxruntime as rt
 
 from pathlib import Path
+import time
 import yaml
 import numpy as np
 
@@ -16,21 +17,23 @@ class AI:
  
         self.output_name = self.sess.get_outputs()[0].name
         self.input_name = self.sess.get_inputs()[0].name
+        # averaging
+        self.queue_length = 10
+        self.last_frames = [(0.0, 0.0) for _ in range(self.queue_length)]
 
     def preprocess(self, img: np.ndarray) -> np.ndarray:
+
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        img = np.transpose(img, (2, 0, 1))
-        img = img.astype(np.float32) / 255
-
-        mean = np.array([0.485, 0.456, 0.406])
-        std = np.array([0.229, 0.224, 0.225])
-
-        img = (img - mean) / std
-
-        return np.array([img], dtype=np.float32)
+        img = cv2.resize(img, (224, 224))
+        img = img.transpose(2, 0, 1)
+        img = np.expand_dims(img, axis=0)
+        img = img.astype(np.float32)
+        return img
 
     def postprocess(self, detections: np.ndarray) -> np.ndarray:
-        return detections[0]
+        # make sure the output is between -1 and 1
+        detections = np.clip(detections, -1.0, 1.0)
+        return detections
 
     def predict(self, img: np.ndarray) -> np.ndarray:
         inputs = self.preprocess(img)
@@ -43,10 +46,30 @@ class AI:
 
         assert outputs.dtype == np.float32
         assert outputs.shape == (2,)
+        # Implement some start driving weightings.
+        # First: exponential average of the last 10 frames
+        self.update_queue(outputs)
+        averaged_outputs = self.exponential_average(alpha=1.2) # Could be a parameter in config, TODO
+
+
         assert outputs.max() < 1.0
         assert outputs.min() > -1.0
 
-        return outputs
+        return averaged_outputs
+    
+    def update_queue(self, new_prediction):
+        self.last_frames.pop(0)
+        self.last_frames.append(new_prediction)
+
+    def exponential_average(self, alpha=1.2):
+        weights = np.array([alpha**i for i in range(self.queue_length)])
+        weights /= weights.sum()
+        weighted_predictions = (np.array(self.last_frames) * weights[:, np.newaxis]).sum(axis=0)
+
+        return weighted_predictions.astype(np.float32)
+    
+
+
 
 
 def main():
@@ -62,27 +85,26 @@ def main():
     video_capture = cv2.VideoCapture(gstreamer_pipeline(flip_method=0, display_width=224, display_height=224), cv2.CAP_GSTREAMER)
 
     # model warm-up
-    ret, image = video_capture.read()
-    if not ret:
-        print(f'No camera')
-        return
-    
-    _ = ai.predict(image)
+    # taken from https://github.com/JanekDev/RoboticsII-Jetbot/blob/dev/src/bot_driving.py
+    WARMUP_FRAMES = 30
+    print("Warming up...")
+    warmup_tic = time.time()
+    for _n in range(WARMUP_FRAMES):
+        ret, image = video_capture.read() 
+        if not ret:
+            print('No camera')
+            return
+        forward, left = ai.predict(image)
+    warmup_tac = time.time()
+    print(f" [ok] Took {warmup_tac - warmup_tic} seconds "
+          f"({((warmup_tac - warmup_tic) * 1000 / WARMUP_FRAMES):.3f} ms per frame)")
 
     input('Robot is ready to ride. Press Enter to start...')
-
-    forward_predictions = [0.0]
-    left_predictions = [0.0]
-
-
-    pred_length = 10
-
-    weights = [np.exp(i) for i in range(pred_length)]
 
     forward, left = 0.0, 0.0
     while True:
         print(f'Forward: {forward:.4f}\tLeft: {left:.4f}')
-        driver.update(np.average(forward_predictions, weights=weights[0:len(forward_predictions)]), np.average(left_predictions, weights=weights[0:len(forward_predictions)]))
+        driver.update(forward, left)
 
         ret, image = video_capture.read()
         if not ret:
@@ -90,14 +112,6 @@ def main():
             break
         forward, left = ai.predict(image)
 
-        forward_predictions.append(forward)
-        left_predictions.append(left)
-
-        if len(forward_predictions) > pred_length:
-            forward_predictions.pop(0)
-            left_predictions.pop(0)
-
 
 if __name__ == '__main__':
     main()
-
